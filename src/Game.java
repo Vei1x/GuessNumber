ab4375caf6718cad4825e9fe95d42573fcabe8ce
Vei1x <nukleon13@gmail.com>
2020-10-29 00:27:58 +0200

    added Min-Max

--- src/Game.java
+++ src/Game.java
@@ -3,18 +3,27 @@ import java.util.Random;
 public class Game {
     public void play() {
         Input input = new Input();
-        int max = input.getMax();
+
         int min = input.getMin();
+        int max = input.getMax();
+
+        while (max < min) {
+            System.out.println("error");
+            min = input.getMin();
+            max = input.getMax();
+        }
 
         Random rand = new Random();
         rand.nextInt();
-        int number = (int)(Math.random() * (max - min + 1) + min);
+        int number = (int) (Math.random() * (max - min + 1) + min);
 
-        System.out.print("Guess the number: ");
+        System.out.print("\nGuess the number: ");
         int guess = -1;
         while (guess != number) {
             guess = input.getGuess();
-            if (guess < number)
+            if (guess > max || guess < min)
+                System.out.print("Number is out of range(" + min + " : " + max + ")\nEnter number: ");
+            else if (guess < number)
                 System.out.print("Your number is too low\nEnter number: ");
             else if (guess > number)
                 System.out.print("Your number is too high\nEnter number: ");
--- src/Input.java
+++ src/Input.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class Input {
     private final Scanner sc = new Scanner(System.in);
 
+    public int getMin() {
+        System.out.print("\nEnter min value: ");
+        return getGuess();
+    }
+
     public int getMax() {
         System.out.print("Enter max value: ");
         return getGuess();
     }
 
-    public int getMin() {
-        System.out.print("Enter min value: ");
-        return getGuess();
-    }
-
     public int getGuess() {
-
         while (true) {
             if (!sc.hasNextInt()) {
-                System.out.print("Value has wrong type of symbol" +
+                System.out.print("\nValue has wrong type of symbol" +
                         "\n(value is not a number)" +
-                        "\nEnter max value:");
+                        "\nRe-enter value:");
                 sc.next();
             } else {
                 return sc.nextInt();
